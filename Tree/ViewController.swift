1ebe357510ce234c8e76cad61384eee354d93f1a
Aldo Martinez <aldo.martinez@globant.com>
2018-12-17 17:40:08 -0600

    Initial Commit

---
+++ Tree/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tree
+//
+//  Created by Aldo Antonio Martinez Avalos on 12/17/18.
+//  Copyright © 2018 Globant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
